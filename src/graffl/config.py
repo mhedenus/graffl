6ca9406c394d2341bada7c3a2cab4fb01e11ed47
mhedenus <michael@hedenus.de>
2026-04-08 23:38:22 +0200

    update

--- src/graffl/config.py
+++ src/graffl/config.py
@@ -13,6 +13,9 @@ class GrafflConfig:
             "type": "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
             "Property": "http://www.w3.org/1999/02/22-rdf-syntax-ns#Property",
 
+            "label": "http://www.w3.org/2000/01/rdf-schema#label",
+            "comment": "http://www.w3.org/2000/01/rdf-schema#comment",
+
             "Class": "http://www.w3.org/2002/07/owl#Class",
             "subClassOf": "http://www.w3.org/2000/01/rdf-schema#subClassOf",
 
